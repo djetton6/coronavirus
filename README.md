ce7a3017a2cb7236c856c5b6a813635f89770467
Desmend Jetton <desmendjetton@gmail.com>
2020-03-23 23:10:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# coronavirus
+ COVID-19 Data viz using Deck GL
---
+++ src/index.js
@@ -0,0 +1,6 @@
+window.initMap = () => {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.0, lng: -100.0 },
+    zoom: 5
+  });
+};
